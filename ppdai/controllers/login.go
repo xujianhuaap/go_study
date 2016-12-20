efb1fec3a596967d7c3628c5c675f24a56dcf9cc
xujianhua <xujianhuaap@gmail.com>
2016-12-20 19:54:56 +0800

    mysql success

--- ppdai/controllers/login.go
+++ ppdai/controllers/login.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 	"github.com/xujianhuaap/gostudy/ppdai/api"
 	"github.com/astaxie/beego/session"
-	"path/filepath"
+	"database/sql"
 )
 var globalSessions *session.Manager
 type LoginController struct {
@@ -14,14 +14,14 @@ type LoginController struct {
 
 func ( c *LoginController) Get()  {
 
-	sess,_:=globalSessions.SessionStart(c.Ctx.ResponseWriter,c.Ctx.Request)
-	defer sess.SessionRelease(c.Ctx.ResponseWriter)
-	username:=sess.Get("username")
-	if(username==nil){
-
-	}else {
-		sess.Set("username",c.Ctx.Request.Form["username"])
-	}
+	//sess,_:=globalSessions.SessionStart(c.Ctx.ResponseWriter,c.Ctx.Request)
+	//defer sess.SessionRelease(c.Ctx.ResponseWriter)
+	//username:=sess.Get("username")
+	//if(username==nil){
+	//
+	//}else {
+	//	sess.Set("username",c.Ctx.Request.Form["username"])
+	//}
 	var url=c.Ctx.Request.URL.Path
 	if strings.EqualFold(url,api.LOGIN){
 		c.TplName="login.html"
@@ -31,44 +31,54 @@ func ( c *LoginController) Get()  {
 }
 
 func (c *LoginController)Post()  {
-	var url=c.Ctx.Request.URL.Path
-	if strings.EqualFold(url,api.LOGIN_AUTH){
-		username:=c.GetString("user_name","")
-		userpassword:=c.GetString("user_password","")
-		if(strings.EqualFold(username,"xu")&& strings.EqualFold(userpassword,"123456")){
-			c.Ctx.WriteString("登录成功")
-		}else {
-			c.Ctx.Redirect(200,api.LOGIN)
-		}
-	}else if strings.EqualFold(url,api.REGISTER_SUBNIT){
-		username:=c.GetString("user_name","")
-		userpassword:=c.GetString("user_password","")
+	db, err:= sql.Open("mysql", "root:123456@/ppdai")
+	if err==nil{
+		var url=c.Ctx.Request.URL.Path
+		if strings.EqualFold(url,api.LOGIN_AUTH){
+			username:=c.GetString("user_name","")
+			userpassword:=c.GetString("user_password","")
+			if(strings.EqualFold(username,"xu")&& strings.EqualFold(userpassword,"123456")){
+				c.Ctx.WriteString("登录成功")
+			}else {
+				c.Ctx.Redirect(200,api.LOGIN)
+			}
+		}else if strings.EqualFold(url,api.REGISTER_SUBNIT){
+			username:=c.GetString("user_name","")
+			userpassword:=c.GetString("user_password","")
+			stmt,err:=db.Prepare("INSERT user SET name=?,password=?,age=?,gender=?")
+			if(err!=nil){
+				c.Ctx.WriteString(err.Error())
+			}else {
+				_,err=stmt.Exec(username,userpassword,26,1)
 
-		if(len(username)>0&& len(userpassword)>8){
-			c.Ctx.WriteString("注册成功")
-		}else {
-			c.Ctx.WriteString("注册失败")
+				if(err!=nil){
+					c.Ctx.WriteString("注册失败")
+				}else {
+					c.Ctx.WriteString("注册成功")
+				}
+
+			}
 		}
+	}else {
+		c.Ctx.WriteString("22222")
 	}
-
-
 }
 
 
-func init() {
-
-		conf := new(session.ManagerConfig)
-
-		conf.CookieName = beego.BConfig.WebConfig.Session.SessionName
-		conf.EnableSetCookie = beego.BConfig.WebConfig.Session.SessionAutoSetCookie
-		conf.Gclifetime = beego.BConfig.WebConfig.Session.SessionGCMaxLifetime
-		conf.Secure = beego.BConfig.Listen.EnableHTTPS
-		conf.CookieLifeTime = beego.BConfig.WebConfig.Session.SessionCookieLifeTime
-		conf.ProviderConfig = filepath.ToSlash(beego.BConfig.WebConfig.Session.SessionProviderConfig)
-		conf.Domain = beego.WebConfig.Session.SessionDomain
-		conf.EnableSidInHttpHeader = beego.BConfig.WebConfig.Session.EnableSidInHttpHeader
-		conf.SessionNameInHttpHeader = beego.BConfig.WebConfig.Session.SessionNameInHttpHeader
-		conf.EnableSidInUrlQuery = beego.BConfig.WebConfig.Session.EnableSidInUrlQuery
-		globalSessions, _ = session.NewManager("mysql", conf)
-		go globalSessions.GC()
-	}
+//func init() {
+//
+//		//conf := new(session.ManagerConfig)
+//		//
+//		//conf.CookieName = beego.BConfig.WebConfig.Session.SessionName
+//		//conf.EnableSetCookie = beego.BConfig.WebConfig.Session.SessionAutoSetCookie
+//		//conf.Gclifetime = beego.BConfig.WebConfig.Session.SessionGCMaxLifetime
+//		//conf.Secure = beego.BConfig.Listen.EnableHTTPS
+//		//conf.CookieLifeTime = beego.BConfig.WebConfig.Session.SessionCookieLifeTime
+//		//conf.ProviderConfig = filepath.ToSlash(beego.BConfig.WebConfig.Session.SessionProviderConfig)
+//		////conf.Domain = beego.WebConfig.Session.SessionDomain
+//		//conf.EnableSidInHttpHeader = beego.BConfig.WebConfig.Session.EnableSidInHttpHeader
+//		//conf.SessionNameInHttpHeader = beego.BConfig.WebConfig.Session.SessionNameInHttpHeader
+//		//conf.EnableSidInUrlQuery = beego.BConfig.WebConfig.Session.EnableSidInUrlQuery
+//		//globalSessions, _ = session.NewManager("mysql", conf)
+//
+//	}
--- ppdai/main.go
+++ ppdai/main.go
@@ -4,29 +4,36 @@ import (
 	"github.com/astaxie/beego"
 
 	_"github.com/xujianhuaap/gostudy/ppdai/routers"
+	 _ "github.com/go-sql-driver/mysql"
+
+	"database/sql"
 )
+
+var db *sql.DB
 func main() {
 	beego.Run()
+
 
 }
 
 func init() {
+
 	beego.BConfig.WebConfig.EnableXSRF=false
 	beego.BConfig.WebConfig.XSRFKey="ppdai123456"
 	beego.BConfig.WebConfig.XSRFExpire=3600
 	beego.BConfig.WebConfig.DirectoryIndex=true
 	beego.BConfig.WebConfig.ViewsPath="views"
 
-	beego.AppConfig.String("mysqluser")="xujianhua"
-	beego.AppConfig.String("mysqlpass")="123456"
-	beego.AppConfig.String("mysqlurl")="127.0.0.1"
-	beego.AppConfig.String("mysqldb")="ppdai"
+	//beego.AppConfig.String("mysqluser")="xujianhua"
+	//beego.AppConfig.String("mysqlpass")="123456"
+	//beego.AppConfig.String("mysqlurl")="127.0.0.1"
+	//beego.AppConfig.String("mysqldb")="ppdai"
 
-	beego.BConfig.WebConfig.Session.SessionOn=true
-	beego.BConfig.WebConfig.Session.SessionProvider="mysql"
-	beego.BConfig.WebConfig.Session.SessionProviderConfig="xujianhua:123456@localhost/ppdai?charset=utf8"
-	beego.BConfig.WebConfig.Session.SessionCookieLifeTime=0//用户关闭浏览器的时候就可以关闭cookie了
-
+	//beego.BConfig.WebConfig.Session.SessionOn=true
+	//beego.BConfig.WebConfig.Session.SessionProvider="mysql"
+	//beego.BConfig.WebConfig.Session.SessionProviderConfig="xujianhua:123456@localhost/ppdai?charset=utf8"
+	//beego.BConfig.WebConfig.Session.SessionCookieLifeTime=0//用户关闭浏览器的时候就可以关闭cookie了
+	//beego.BConfig.WebConfig.Session.SessionDomain="xjh"
 
 	beego.SetStaticPath("/apk","apk")
 
